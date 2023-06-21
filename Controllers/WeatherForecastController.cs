5071f9bc83427bee060f3fa511fa8520c6051e49
satyapsr13 <satyapsr13@gmail.com>
2023-06-21 15:49:20 +0530

    changes in git file

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -29,5 +29,17 @@ namespace Dotnet_REST_API.Controllers
             })
             .ToArray();
         }
+
+        [HttpGet(Name = "test_route")]
+        public IEnumerable<WeatherForecast> TestingFunction()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
     }
 }
